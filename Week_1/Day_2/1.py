a11645412f1db169121350e8c88ccaabfa89b319
Kanan Mehta <kananmuskanmehta@gmail.com>
2026-06-02 22:18:34 +0530

    Day_2

---
+++ Week_1/Day_2/1.py
@@ -0,0 +1,8 @@
+#1.Maximum Subarray
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        max_sum = current_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
